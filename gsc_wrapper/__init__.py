ac2d5e9fca443d48b5a4cddb0f4a249a5fc49491
Andrea Moro <info@andreamoro.eu>
2023-10-31 08:51:03 +0100

    Added Google's URLs for token authorization, remove, grant

--- gsc_wrapper/__init__.py
+++ gsc_wrapper/__init__.py
@@ -2,5 +2,19 @@ from gsc_wrapper.account import Account, WebProperty
 from gsc_wrapper.query import Query, Report
 from gsc_wrapper.enums import country, data_state, dimension, search_type, operator
 
-__all__ = ('Account', 'WebProperty', 'Query', 'Report', 'country', 'data_state', 'dimension', 'search_type', 'operator')
-__version__ = '1.0.0'
+__all__ = (
+    "Account",
+    "WebProperty",
+    "Query",
+    "Report",
+    "country",
+    "data_state",
+    "dimension",
+    "search_type",
+    "operator",
+)
+__version__ = "1.0.0"
+
+GOOGLE_AUTH_URI = "https://accounts.google.com/o/oauth2/v2/auth"
+GOOGLE_REVOKE_URI = "https://oauth2.googleapis.com/revoke"
+GOOGLE_TOKEN_URI = "https://oauth2.googleapis.com/token"
